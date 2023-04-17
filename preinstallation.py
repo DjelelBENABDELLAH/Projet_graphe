9b4de36fe3e0c0dea6844dad7f2a98211f6c4104
DjelelBENABDELLAH <103830576+DjelelBENABDELLAH@users.noreply.github.com>
2023-04-17 15:11:13 +0200

    Update preinstallation.py

--- preinstallation.py
+++ preinstallation.py
@@ -1,2 +1,22 @@
+import os
 
-pip install certifi==2022.12.7 charset-normalizer==2.1.1 click==8.1.3 community==1.0.0b1 contourpy==1.0.6 cycler==0.11.0 Cython==0.29.33 decorator==5.1.1 fa2==0.3.5 Flask==2.2.2 fonttools==4.38.0 graphviz==0.20.1 idna==3.4 itsdangerous==2.1.2 Jinja2==3.1.2 kiwisolver==1.4.4 MarkupSafe==2.1.1 matplotlib==3.5.1 networkx==2.5 numpy==1.24.1 packaging==23.0 Pillow==9.4.0 pydot==1.4.2 pygraphviz==1.10 pyparsing==3.0.9 python-dateutil==2.8.2 python-louvain==0.16 requests==2.28.1 scipy==1.10.0 setuptools==65.5.0 six==1.16.0 tqdm==4.64.1 urllib3==1.26.13 Werkzeug==2.2.2
+# Installer pygraphviz
+os.system("pip install pygraphviz==1.10")
+
+# Installer community
+os.system("pip install python-louvain==0.16")
+
+# Installer networkx
+os.system("pip install networkx==2.5")
+
+# Installer numpy
+os.system("pip install numpy==1.24.1")
+
+# Installer matplotlib
+os.system("pip install matplotlib==3.5.1")
+
+# Installer forceatlas2
+os.system("pip install fa2==0.3.5")
+
+# Installer mpl_toolkits
+os.system("pip install mplot3d")
